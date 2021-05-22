afbaa909554351b8f29da1ee7b26ca7b815681fa
rudrashan16 <74367420+rudrashan16@users.noreply.github.com>
2021-05-22 07:15:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,94 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+  var score;
+  var particle;
+var particles = [];
+var plinkos = [];
+var divisions =[];
+var divisionHeight=300;
+var score =0;
+ var gameState,count;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+  for (var k = 0; k <=width; k =k+80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
+ 
+}
+
+function draw() {
+  background("black");
+  textSize(20)
+ text("Score : "+score,20,30);
+ text("500 ",100,540);
+ text("500 ",20,540);
+ text("500 ",180,540);
+ text("500 ",260,540);
+ text("100 ",340,540);
+ text("100 ",420,540);
+ text("100 ",500,540);
+ text("200 ",580,540);
+ text("200 ",660,540);
+ text("200 ",740,540);
+
+ 
+ mousePressed();
+  Engine.update(engine);
+  ground.display();
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   if(frameCount%60===0){
+     particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+     score++;
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+
+   
+
+  }
